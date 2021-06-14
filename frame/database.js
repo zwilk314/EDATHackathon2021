6d433ebd3410f60495f92a71c0561d77ae7e3c6f
zwilk314 <zwilkerson314@gmail.com>
2021-06-14 19:59:25 -0400

    Some fixes for login procedures (everything should work now) - ready to begin layering functionality

--- frame/database.js
+++ frame/database.js
@@ -9,7 +9,21 @@ var javascriptKey = "JIMPkxp1LnuZytN4t9DtZkQ4TFmlshEO2eiMHY85"
 var serverID = "https://parseapi.back4app.com"
 
 //Takes username and password and adds new user account to the database with that information
+// function addNewUser(username, password){
+// 	Parse.initialize(applicationID, javascriptKey)
+// 	Parse.serverURL = serverID
+// 	var user = new Parse.User()
+// 	user.set("username", username)
+// 	user.set("password", password)
+// 	user.set("Groups" , "")
+// 	user.signUp().then(function(user){
+// 		alert("New user " + username + " successfully added.")
+// 	})
+// }
+
 function addNewUser(username, password){
+	Parse.initialize(applicationID, javascriptKey)
+	Parse.serverURL = serverID
 	var entry = [
 		username,
 		password,
@@ -20,18 +34,18 @@ function addNewUser(username, password){
 		"password",
 		"Groups"
 	]
-	Parse.initialize(applicationID, javascriptKey)
-	Parse.serverURL = serverID
-	var nImport = Parse.Object.extend("User")
+	var nImport = Parse.Object.extend("customUser")
 	var newEntry = new nImport()
 	for(i=0; i<titles.length; i++){
 		newEntry.set(titles[i], entry[i])
 		if(i == (titles.length - 1)){
 			newEntry.save().then(
-				(result) => {
-					alert("New user " + username + " successfully added.")
+				function(result){
+					alert("Added " + result.get("username") + " successfully.")
 				}
-			)
+			).catch(function(error){
+				alert(error)
+			})
 		}
 	}
 }
@@ -41,20 +55,20 @@ function addNewUser(username, password){
 function loginUser(username, password){
 	Parse.initialize(applicationID, javascriptKey)
 	Parse.serverURL = serverID
-	var authorization = new Parse.Query(Parse.Object.extend("User"))
+	var query = new Parse.Query(Parse.Object.extend("customUser"))
 	query.equalTo("username", username)
 	query.equalTo("password", password)
-	query.find().then(result => {
+	query.find().then(function(result){
 		if(result.length == 0){
 			alert("Invalid username or password")
 			return
 		}
 		else{
-			alert("Successful login")
+			alert("Successful login " + result[0].get("username"))
 			//window.open("???")
 			//TODO: navigate to home page
-			sessionStorage.setItem("username", result.get("username"))
-			sessionStorage.setItem("Groups", result.get("Groups"))
+			//sessionStorage.setItem("username", result.get("username"))
+			//sessionStorage.setItem("Groups", result.get("Groups"))
 		}
 	})
 }
@@ -80,13 +94,14 @@ function addNewGroup(username, groupName, groupDescription){
 		newEntry.set(titles[i], entry[i])
 		if(i == (titles.length - 1)){
 			newEntry.save().then(
-				(result) => {
-					var query = new Parse.Query(Parse.Object.extend("User"))
+				function(result){
+					alert(result)
+					var query = new Parse.Query(Parse.Object.extend("customUser"))
 					query.equalTo("username", username)
 					query.find().then(result => {
 						var oldList = result.get("Groups")
 						var id = result.get("objectId")
-						nImport = Parse.Object.extend("User")
+						nImport = Parse.Object.extend("customUser")
 						newEntry = new nImport()
 						newEntry.get(id).then((result) => {
 							result.set("Groups", oldList + "," + groupName)
@@ -150,7 +165,7 @@ function addNewMeeting(groupName, meetingInfoString){
 		else{
 			var oldList = result.get("Meetings")
 			var id = result.get("objectId")
-			var nImport = Parse.Object.extend("User")
+			var nImport = Parse.Object.extend("customUser")
 			var newEntry = new nImport()
 			newEntry.get(id).then((result) => {
 				result.set("Meetings", oldList + "," + meetingInfoString)
--- login/newUser.js
+++ login/newUser.js
@@ -1,9 +1,15 @@
 //Javascript support for login.html
 
+// function createNewUser(){
+// 	var name = document.getElementById("newUserName").value
+// 	var desc = document.getElementById("newUserPassword").value
+// 	file = fopen(getScriptPath(), 3)
+// 	fwrite(file, "hello world") // The account storage database
+//     
+// }
+
 function createNewUser(){
-	var name = document.getElementById("newUserName").value
-	var desc = document.getElementById("newUserPassword").value
-	file = fopen(getScriptPath(), 3)
-	fwrite(file, "hello world") // The account storage database
-    
+	var name = document.getElementById("username").value
+    var pwd = document.getElementById("password").value
+    addNewUser(name, pwd)
 }
